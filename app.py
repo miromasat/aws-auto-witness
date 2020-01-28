50cdc51f3be75be7a3170ece2b624c7d0e94d4b1
Masat <mmmasat@a483e7dd7ec3.ant.amazon.com>
2020-01-28 14:56:00 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from amazon_auto_witness.amazon_auto_witness_stack import AmazonAutoWitnessStack
+
+
+app = core.App()
+AmazonAutoWitnessStack(app, "amazon-auto-witness")
+
+app.synth()
