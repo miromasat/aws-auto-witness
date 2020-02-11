dadfc4494396731d96e5971674c7d53650ac24fa
Masat <miro.masat@gmail.com>
2020-02-11 15:35:14 -0800

    added lambda and step functions cdk constructs

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 ![AutoWitness Architecture](AutoWitnessArchitectureEXPORT.png?raw=true "AutoWitness Architecture")
 ### The mechanics of the architecture are described below:
 1. Architecture awaits ingestion into `Archive` S3 bucket either from the video archive or from live cameras. Videos need to land in `Archive` S3 bucket in one of supported formats.
-2. `Archive` S3 bucket upon the object landing triggers a notification to `Feeder` Lambda function that lies within Step Function Workflow that kicks in and manages rest of the labelling.
-3. 'Feeder' Lambda function reads and loads `Preferences` DynamoDB table, that describes, what is required of the `Label` Rekognition task and what metadata should be tracked and recorded.
+2. `Archive` S3 bucket upon the object landing triggers a notification to `Feeder` Lambda function that lies within `Orchestrator` Step Function Workflow that kicks in and manages the rest of the labelling and storing the result.
+3. `Feeder` Lambda function reads and loads `Preferences` DynamoDB table, that describes, what is required of the `Label` Rekognition task and what metadata should be tracked and recorded.
 4. `Label` Rekognition task runs and returns the finished metadata and spatial representation of the scene to the `Saver` Lambda function.
 5. `Saver` Lambda function populates `Data` S3 bucket with searchable metadata and `Clip` S3 bucket with processed video clips that contain features only.
 6. `Witness` SageMaker Notebook is set up and equipped with all libraries that allow to consume and query both `Data` S3 bucket and `Clips` S3 bucket.
--- amazon_auto_witness/amazon_auto_witness_stack.py
+++ amazon_auto_witness/amazon_auto_witness_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core, aws_dynamodb, aws_s3, aws_athena
+from aws_cdk import core, aws_dynamodb, aws_s3, aws_athena, aws_stepfunctions, aws_lambda
 
 
 class AmazonAutoWitnessStack(core.Stack):
@@ -8,11 +8,38 @@ class AmazonAutoWitnessStack(core.Stack):
 
 
         archive = aws_s3.Bucket(self, 
-                                        id='_archive')
-        datastore = aws_s3.Bucket(self, 
-                                        id='_datastore')
-
-        configuration = aws_dynamodb.Table(self,    id='_configuration', 
-                                                    table_name='witness_configuration', 
-                                                    partition_key=aws_dynamodb.Attribute(name='parameter', type='STRING'))
+                                                    id='_archive')
+        data = aws_s3.Bucket(self, 
+                                                    id='_data')
+        clips = aws_s3.Bucket(self, 
+                                                    id='_clips')                                
+        preferences = aws_dynamodb.Table(self,      id='_preferences', 
+                                                    table_name='witness_preferences', 
+                                                    partition_key=aws_dynamodb.Attribute(name='preference_name', type='STRING'))
         
+        # space for athena manifests and queries
+
+        # space for stepfunction
+        orchestrator = aws_stepfunctions.StateMachine(self,
+                                                    id='_orchestrator',
+                                                    state_machine_name='witness_orchestrator')
+        # space for feeder Lambda function
+        feeder = aws_lambda.Function(self,
+                                                    id='_feeder',
+                                                    code='return True',
+                                                    handler='function.py',
+                                                    runtime='python3.7',
+                                                    description='Feeder function for the Witness project')
+
+        # space for saver Lambda function
+        saver = aws_lambda.Function(self,
+                                                    id='saver',
+                                                    code='return True',
+                                                    handler='function.py',
+                                                    runtime='python3.7',
+                                                    description='Saver function for the Witness project')
+
+        # space for sagemaker notebook
+
+        # space for iam roles
+ 
